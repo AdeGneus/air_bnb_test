933a0028aae8f8a337e87085575c8f8b8f47eda1
Nimah <busayonimota1603@gmail.com>
2022-08-03 20:54:25 +0100

    Implements the console.py

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""An interactive shell?"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)  '
+
+    def do_EOF(self, line):
+        print("")
+        return True
+
+    def do_quit(self, line):
+        print("Bye bye ooo")
+        return True
+
+    def do_empty_line(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
